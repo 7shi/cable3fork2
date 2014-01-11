c74a3973259d092a358e53f3e53cfda137642f2b
7shi <7shi@live.jp>
2014-01-11 10:50:31 +0900

    フラグをマクロ化: --|--|--|--|OF|DF|IF|TF|SF|ZF|--|AF|--|PF|--|CF

--- cable3.c
+++ cable3.c
@@ -24,6 +24,7 @@ SDL_Surface *k = 0;
 #define CH r8[5]
 #define DH r8[6]
 #define BH r8[7]
+
 #define AX r[0]
 #define CX r[1]
 #define DX r[2]
@@ -33,9 +34,20 @@ SDL_Surface *k = 0;
 #define SI r[6]
 #define DI r[7]
 #define ES r[8]
+
 #define CS r[9]
 #define SS r[10]
 #define DS r[11]
+
+#define CF r8[40]
+#define PF r8[41]
+#define AF r8[42]
+#define ZF r8[43]
+#define SF r8[44]
+#define TF r8[45]
+#define IF r8[46]
+#define DF r8[47]
+#define OF r8[48]
 
 int
 K(int o)
@@ -52,14 +64,14 @@ D(int o)
 int
 w(int o)
 {
-	return r[o] += ~(-2 * r8[47]) * ~L;
+	return r[o] += ~(-2 * DF) * ~L;
 }
 
 int
 v(int o)
 {
 	z((f ^= S ^ N) & 16);
-	return G(N - S && 1 & (r8[40] ^ f >> 8 * -~L - 1));
+	return G(N - S && 1 & (CF ^ f >> 8 * -~L - 1));
 }
 
 int fbits[] = {0, 2, 4, 6, 7, 8, 9, 10, 11};
@@ -73,6 +85,13 @@ getflags(void)
 	return f;
 }
 
+void
+setflags(int f)
+{
+	for (int i = 0; i < 9; ++i)
+		r8[40 + i] = !!(f & (1 << fbits[i]));
+}
+
 int
 intr(int n)
 {
@@ -83,14 +102,7 @@ intr(int n)
 	PUSH(ip);
 	POKE(CS, =, mem[4 * n + 2]);
 	POKE(ip, =, mem[4 * n]);
-	return (r8[46] = 0);
-}
-
-int
-s(int o)
-{
-	for (O = 9; O--;)
-		r8[40 + O] = 1 && 1 << D(25) & o;
+	return (IF = 0);
 }
 
 int
@@ -103,7 +115,7 @@ main(int argc, char *argv[])
 		j[--q] = *++argv ? open(*argv, 0x8082) : 0;
 	*(uint32_t *) r = *j ? lseek(*j, 0, 2) >> 9 : 0;
 	read(j[2], &mem[P + ip], P);
-	for (O = 9; Y = mem + 16 * CS + ip, Y - mem; Q | R || kb & r8[46] && KB) {
+	for (O = 9; Y = mem + 16 * CS + ip, Y - mem; Q | R || kb & IF && KB) {
 		L = (X = *Y & 7) & 1;
 		o = X / 2 & 1;
 		O = l[32] = 0;
@@ -156,7 +168,7 @@ main(int argc, char *argv[])
 		case 8:
 			13[W = h, r] = (o |= !L) ? (int8_t) d : d, U = P + 26, ip -= ~!o, u = 17 + (m = a);
 		case 9:
-			(a = m), a-- || (POKE(mem[W], +=, mem[U]), F(N < S)), a-- || (POKE(mem[W], |=, mem[U])), a-- || (v(F(40[POKE(mem[W], +=r8[40] +, mem[U]), r8] & N == S | +N < +(int) S))), a-- || (v(F(40[POKE(mem[W], -=r8[40] +, mem[U]), r8] & N == S | -N < -(int) S))), a-- || (POKE(mem[W], &=, mem[U])), a-- || (POKE(mem[W], -=, mem[U]), F(N > S)), a-- || (POKE(mem[W], ^=, mem[U])), a-- || (POKE(mem[W], -, mem[U]), F(N > S)), a-- || (POKE(mem[W], =, mem[U]));
+			(a = m), a-- || (POKE(mem[W], +=, mem[U]), F(N < S)), a-- || (POKE(mem[W], |=, mem[U])), a-- || (v(F(40[POKE(mem[W], +=CF +, mem[U]), r8] & N == S | +N < +(int) S))), a-- || (v(F(40[POKE(mem[W], -=CF +, mem[U]), r8] & N == S | -N < -(int) S))), a-- || (POKE(mem[W], &=, mem[U])), a-- || (POKE(mem[W], -=, mem[U]), F(N > S)), a-- || (POKE(mem[W], ^=, mem[U])), a-- || (POKE(mem[W], -, mem[U]), F(N > S)), a-- || (POKE(mem[W], =, mem[U]));
 			break;
 		case 10:
 			!L ? L = a += 8, A = 4 * !T, O = t, W = h = T < 3 ? 16 * r[Q ? p : D(A + 3)] + (uint16_t) (D(A + 1)[r] + D(A + 2) * g + r[D(A)]) : K(t), U = flags = K(a), o ? U = h, W = flags : flags, POKE(mem[W], =, mem[U]) : !o ? Q = 1, POKE((mem[p = m, A = 4 * !T, O = t, W = h = T < 3 ? 16 * r[Q ? p : D(A + 3)] + (uint16_t) (D(A + 1)[r] + D(A + 2) * g + r[D(A)]) : K(t), U = flags = K(a), o ? U = h, W = flags : flags, flags]), =, h) : POP(h[mem]);
@@ -165,7 +177,7 @@ main(int argc, char *argv[])
 			T = a = 0, t = 6, g = c, A = 4 * !T, O = t, W = h = T < 3 ? 16 * r[Q ? p : D(A + 3)] + (uint16_t) (D(A + 1)[r] + D(A + 2) * g + r[D(A)]) : K(t), U = flags = K(a), o ? U = h, W = flags : flags, POKE(mem[U], =, mem[W]);
 			break;
 		case 12:
-			(A = (1 & (L ? *(int16_t *) & h[mem] : h[mem]) >> 8 * -~L - 1), flags = m ? ++ip, (int8_t) g : o ? 31 & DL : 1) && (a < 4 ? flags %= a / 2 + 8 * -~L, POKE(A, =, h[mem]) : 0, a & 1 ? POKE(h[mem], >>=, flags) : POKE(h[mem], <<=, flags), a > 3 ? u = 19 : 0, a < 5 ? 0 : F(S >> flags - 1 & 1)), a-- || (POKE(h[mem], +=, A >> 8 * -~L - flags), G((1 & (L ? *(int16_t *) & N : N) >> 8 * -~L - 1) ^ F(N & 1))), a-- || (A &= (1 << flags) - 1, POKE(h[mem], +=, A << 8 * -~L - flags), G((1 & (L ? *(int16_t *) & N * 2 : N * 2) >> 8 * -~L - 1) ^ F((1 & (L ? *(int16_t *) & N : N) >> 8 * -~L - 1)))), a-- || (POKE(h[mem], +=(r8[40] << flags - 1) +, A >> 1 + 8 * -~L - flags), G((1 & (L ? *(int16_t *) & N : N) >> 8 * -~L - 1) ^ F(A & 1 << 8 * -~L - flags))), a-- || (POKE(h[mem], +=(r8[40] << 8 * -~L - flags) +, A << 1 + 8 * -~L - flags), F(A & 1 << flags - 1), G((1 & (L ? *(int16_t *) & N : N) >> 8 * -~L - 1) ^ (1 & (L ? *(int16_t *) & N * 2 : N * 2) >> 8 * -~L - 1))), a-- || (G((1 & (L ? *(int16_t *) & N : N) >> 8 * -~L - 1) ^ F((1 & (L ? *(int16_t *) & S << flags - 1 : S << flags - 1) >> 8 * -~L - 1)))), a-- || (G((1 & (L ? *(int16_t *) & S : S) >> 8 * -~L - 1))), a-- || (0), a-- || (flags < 8 * -~L || F(A), G(0), POKE(h[mem], +=, A *= ~((1 << 8 * -~L) - 1 >> flags)));
+			(A = (1 & (L ? *(int16_t *) & h[mem] : h[mem]) >> 8 * -~L - 1), flags = m ? ++ip, (int8_t) g : o ? 31 & DL : 1) && (a < 4 ? flags %= a / 2 + 8 * -~L, POKE(A, =, h[mem]) : 0, a & 1 ? POKE(h[mem], >>=, flags) : POKE(h[mem], <<=, flags), a > 3 ? u = 19 : 0, a < 5 ? 0 : F(S >> flags - 1 & 1)), a-- || (POKE(h[mem], +=, A >> 8 * -~L - flags), G((1 & (L ? *(int16_t *) & N : N) >> 8 * -~L - 1) ^ F(N & 1))), a-- || (A &= (1 << flags) - 1, POKE(h[mem], +=, A << 8 * -~L - flags), G((1 & (L ? *(int16_t *) & N * 2 : N * 2) >> 8 * -~L - 1) ^ F((1 & (L ? *(int16_t *) & N : N) >> 8 * -~L - 1)))), a-- || (POKE(h[mem], +=(CF << flags - 1) +, A >> 1 + 8 * -~L - flags), G((1 & (L ? *(int16_t *) & N : N) >> 8 * -~L - 1) ^ F(A & 1 << 8 * -~L - flags))), a-- || (POKE(h[mem], +=(CF << 8 * -~L - flags) +, A << 1 + 8 * -~L - flags), F(A & 1 << flags - 1), G((1 & (L ? *(int16_t *) & N : N) >> 8 * -~L - 1) ^ (1 & (L ? *(int16_t *) & N * 2 : N * 2) >> 8 * -~L - 1))), a-- || (G((1 & (L ? *(int16_t *) & N : N) >> 8 * -~L - 1) ^ F((1 & (L ? *(int16_t *) & S << flags - 1 : S << flags - 1) >> 8 * -~L - 1)))), a-- || (G((1 & (L ? *(int16_t *) & S : S) >> 8 * -~L - 1))), a-- || (0), a-- || (flags < 8 * -~L || F(A), G(0), POKE(h[mem], +=, A *= ~((1 << 8 * -~L) - 1 >> flags)));
 			break;
 		case 13:
 			(flags = !!--1[a = X, r]), a-- || (flags &= !m[r8]), a-- || (flags &= m[r8]), a-- || (0), a-- || (flags = !++CX), ip += flags * (int8_t) c;
@@ -201,7 +213,7 @@ main(int argc, char *argv[])
 			if (!R || CX) {
 				POKE(mem[m ? P : 16 * r[Q ? p : 11] + (uint16_t) (SI)], -, mem[16 * ES + (uint16_t) (DI)]);
 				u = 92;
-				r8[43] = !N;
+				ZF = !N;
 				F(N > S);
 				if (!m)
 					w(6);
@@ -215,7 +227,7 @@ main(int argc, char *argv[])
 			}
 			break;
 		case 19:
-			o = L, POP(ip), m && POP(CS), m & 2 ? s(POP(flags)) : o || (SP += c);
+			o = L, POP(ip), m && POP(CS), m & 2 ? setflags(POP(flags)) : o || (SP += c);
 			break;
 		case 20:
 			POKE(U[mem], =, d);
@@ -239,10 +251,10 @@ main(int argc, char *argv[])
 			Q = 2, p = m, R && R++;
 			break;
 		case 28:
-			L = 0, O = AL, F(D(m += 3 * r8[42] + 6 * r8[40])), z(D(1 + m)), N = AL = D(m - 1);
+			L = 0, O = AL, F(D(m += 3 * AF + 6 * CF)), z(D(1 + m)), N = AL = D(m - 1);
 			break;
 		case 29:
-			AX += 262 * (m - 1) * z(F((AL & 15) > 9 | r8[42]));
+			AX += 262 * (m - 1) * z(F((AL & 15) > 9 | AF));
 			N = AL &= 15;
 			break;
 		case 30:
@@ -262,11 +274,11 @@ main(int argc, char *argv[])
 			PUSH(flags);
 			break;
 		case 34:
-			s(POP(flags));
+			setflags(POP(flags));
 			break;
 		case 35:
 			flags = getflags();
-			s((flags & ~m) + CL);
+			setflags((flags & ~m) + CL);
 			break;
 		case 36:
 			flags = getflags();
@@ -320,7 +332,7 @@ main(int argc, char *argv[])
 		O = u;
 		D(16) ? v(0) : D(17) && G(F(0));
 		ip += (T % 3 + 2 * !(!T * t - 6)) * D(20) + D(18) - D(19) * ~!!L;
-		D(15) ? O = m = N, 41[43[r8[44] = (1 & (L ? *(int16_t *) & N : N) >> 8 * -~L - 1), r8] = !N, r8] = D(50) : 0;
+		D(15) ? O = m = N, 41[43[SF = (1 & (L ? *(int16_t *) & N : N) >> 8 * -~L - 1), r8] = !N, r8] = D(50) : 0;
 		if (!++q) {
 			kb = 1;
 			if (*l) {
@@ -341,17 +353,17 @@ main(int argc, char *argv[])
 int
 F(int o)
 {
-	return r8[40] = !!o;
+	return CF = !!o;
 }
 
 int
 z(int o)
 {
-	return r8[42] = !!o;
+	return AF = !!o;
 }
 
 int
 G(int o)
 {
-	return r8[48] = o;
+	return OF = o;
 }
