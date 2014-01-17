a83b84ca9ccdddf6c939933bc627fe9ec5748c5d
7shi <7shi@live.jp>
2014-01-17 23:29:00 +0900

    生成スクリプトで出力部分を分離

--- table-index.fsx
+++ table-index.fsx
@@ -4,7 +4,8 @@ let start = baseaddr + 0x100
 let last = start + bios.Length
 let read32 addr = System.BitConverter.ToInt32(bios, addr - start)
 
-Seq.initInfinite (fun i -> i, read32 (start + 3 + 4 * i))
+let tables =
+   Seq.initInfinite (fun i -> i, read32 (start + 3 + 4 * i))
 |> Seq.takeWhile (fun (_, addr) -> baseaddr <= addr && addr <= last)
 |> Seq.sortBy (fun (_, addr) -> addr)
 |> fun s -> Seq.append s (seq {yield Seq.length s, last})
@@ -12,5 +13,6 @@ Seq.initInfinite (fun i -> i, read32 (start + 3 + 4 * i))
 |> Seq.map (fun ((i0, ad0), (_, ad1)) ->
     i0, ad0, if ad0 < start then 0 else ad1 - ad0)
 |> Seq.sortBy (fun (index, _, _) -> index)
-|> Seq.iter (fun (index, addr, size) ->
-    printfn "%d\t%x\t%d" index addr size)
+
+for index, addr, size in tables do
+    printfn "%d\t%x\t%d" index addr size
